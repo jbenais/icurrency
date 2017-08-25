8dfed390adb3afd97eeb732cb8ed573302672ce0
Julia Benais <julia.benais@epita.fr>
2017-08-26 01:05:31 +0200

    Design: Updated the design, added a LaunchScreen

--- iCurrency/iCurrency/ViewController.swift
+++ iCurrency/iCurrency/ViewController.swift
@@ -27,29 +27,56 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     
     @IBOutlet weak var resultLabel: UILabel!
     
+    @IBOutlet weak var wrongCurrencies: UILabel!
     @IBAction func convertButton(_ sender: Any)
     {
-        if amount.text == ""
+        if !currencies.contains(toText.text!) || !currencies.contains(fromText.text!)
         {
+            wrongCurrencies.text = "Please choose valid currencies"
+            wrongCurrencies.isHidden = false
+        }
+        else if self.amount.text == ""
+        {
+            alertAmount.text = "Please insert an amount"
             alertAmount.isHidden = false
         }
-        else
+        else if let doubleValue = Double(self.amount.text!)
         {
-            datePicker.isHidden = true;
-            alertAmount.isHidden = true
-            let request = "http://api.fixer.io/latest?base=\(fromText.text! as String)&symbols=\(toText.text! as String)&date=\(date.text! as String)"
-            Alamofire.request(request).responseJSON { response in
-                if let json = response.result.value
-                {
-                    let rate = json as! NSDictionary
-                    let cur = rate["rates"] as! NSDictionary
-                    let value = cur.allValues[0] as! Double * Double(self.amount.text!)!
-                    self.resultLabel.text = String(value)
+            if doubleValue < 0
+            {
+                alertAmount.text = "Please insert a positive amount"
+                alertAmount.isHidden = false
+            }
+
+            else
+            {
+                datePicker.isHidden = true
+                alertAmount.isHidden = true
+                wrongCurrencies.isHidden = true
+                let request = "http://api.fixer.io/latest?base=\(fromText.text! as String)&symbols=\(toText.text! as String)&date=\(date.text! as String)"
+                Alamofire.request(request).responseJSON { response in
+                    let rescode = response.response?.statusCode
+                    if rescode == 200
+                    {
+                        if let json = response.result.value
+                        {
+                            let rate = json as! NSDictionary
+                            let cur = rate["rates"] as! NSDictionary
+                            let value = cur.allValues[0] as! Double * Double(self.amount.text!)!
+                            self.resultLabel.text = String(value)
+                        }
+                    }
+                    else
+                    {
+                        let alert = UIAlertController(title: "Oops! An error occured", message: " It appears that some fields are missing or invalid", preferredStyle: UIAlertControllerStyle.alert)
+                        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+                        self.present(alert, animated: true, completion: nil)
+                        self.datePicker.reloadInputViews()
+
+                    }
                 }
-                
             }
         }
-        
     }
     @IBOutlet weak var alertAmount: UILabel!
     
@@ -95,6 +122,11 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         super.viewDidLoad()
         loadCurrencies()
         datePicker.datePickerMode = UIDatePickerMode.date
+        datePicker.maximumDate = NSDate() as Date
+        var components = DateComponents()
+        components.year = -17
+        let minDate = Calendar.current.date(byAdding: components, to: Date())
+        datePicker.minimumDate = minDate
         datePicker.addTarget(self, action: #selector(ViewController.datePickerValueChanged(sender:)), for: UIControlEvents.valueChanged)
         date.inputView = datePicker
         fromPickerView.delegate = self
@@ -106,6 +138,11 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         fromPickerView.tag = 1
         toPickerView.tag = 2
         alertAmount.isHidden = true
+    }
+    
+    override func viewWillAppear(_ animated: Bool)
+    {
+        self.datePicker.reloadInputViews()
     }
     
     func loadCurrencies()
@@ -128,8 +165,13 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         // Dispose of any resources that can be recreated.
     }
 
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        view.endEditing(true)
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        self.view.endEditing(true)
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(false)
     }
     
     func datePickerValueChanged(sender: UIDatePicker)
