18d263d64dd624f97a46823799fae2c4093d1b8e
Julia Benais <julia.benais@epita.fr>
2017-08-25 11:19:30 +0200

    MISC: Started the iCurrency iOS app

---
+++ iCurrency/iCurrency/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  iCurrency
+//
+//  Created by Julia Benaïs on 25/08/2017.
+//  Copyright © 2017 Julia Benaïs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+
+    @IBOutlet weak var result: UILabel!
+    @IBOutlet weak var fromValue: UITextField!
+    @IBOutlet weak var toValue: UITextField!
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
