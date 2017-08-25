038b734e5d5cce19ef329470fff1143f8413ab50
Julia Benais <julia.benais@epita.fr>
2017-08-25 16:14:13 +0200

    GIT: Alamofire added in git, fixed some bugs

--- iCurrency/iCurrency/ViewController.swift
+++ iCurrency/iCurrency/ViewController.swift
@@ -7,19 +7,64 @@
 //
 
 import UIKit
+import Alamofire
 
-class ViewController: UIViewController
+
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource
 {
-
-    @IBOutlet weak var result: UILabel!
-    @IBOutlet weak var fromValue: UITextField!
-    @IBOutlet weak var toValue: UITextField!
-    @IBOutlet weak var pickerView: UIPickerView!
+    
+    @IBOutlet weak var fromText: UITextField!
+    @IBOutlet weak var toText: UITextField!
+    @IBOutlet weak var amount: UITextField!
+    @IBOutlet weak var date: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBAction func convertButton(_ sender: Any)
+    {
+        // DO THE CONVERSION
+    }
+    
+    let fromPickerView = UIPickerView()
+    let toPickerView = UIPickerView()
+    var currencies:[String] = ["EUR", "USD", "DRH", "LIV", "SHEK"]
+    var values:[Double] = []
+    var activeCurrency : Double = 0
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int
+    {
+        return currencies.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return currencies[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
+    {
+        if (pickerView.tag == 1)
+        {
+            fromText.text = currencies[row]
+        }
+        else
+        {
+            toText.text = currencies[row]
+        }
+        self.view.endEditing(false)
+    }
     
     
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        fromPickerView.delegate = self
+        fromPickerView.dataSource = self
+        toPickerView.delegate = self
+        toPickerView.dataSource = self
+        fromText.inputView = fromPickerView
+        toText.inputView = toPickerView
+        fromPickerView.tag = 1
+        toPickerView.tag = 2
     }
 
     override func didReceiveMemoryWarning() {
